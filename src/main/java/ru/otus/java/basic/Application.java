1248e5dcc31f87e131e030de799eefaaaac0d039
Nikita <kovalev53191@mail.ru>
2026-09-18 11:55:56 +0300

    homework4 done

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -9,9 +9,11 @@ public class Application {
         Animal dog = new Dog("Niko",19.4,1.5,250);
         Animal horse = new Horse("Star",22.2,2.0,450);
 
-        System.out.println("dog " + dog.run(100));
-        System.out.println("horse " + horse.run(100));
-        System.out.println("cat " + cat.run(100));
+        System.out.println("dog " + dog.run(100) + " " + dog.swim(50));
+        System.out.println("horse " + horse.run(100) + " " + horse.swim(50));
+        System.out.println("cat " + cat.run(100) + " " + cat.swim(50));
         cat.info();
+        dog.info();
+        horse.info();
     }
 }
